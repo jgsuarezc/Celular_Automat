f44cfe7a5e490f57417e7e384f02ecdf52db2b1a
jgsuarezc <jgsuarezc@unal.edu.co>
2023-07-12 18:43:52 -0500

    Version preeliminar Juego de la vida

--- Game _Of_Life.cpp
+++ Game _Of_Life.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
+//celda muere
 // Actualiza el espacio segun el estado de calda celda:
-const int N=5;
+const int N=10;
 void Actualiza(int N,int & Espacio[N][N]);
 int main(void) {
   bool Espacio[N][N]={false};
@@ -10,15 +11,20 @@ int main(void) {
 }
 
 void Actualiza(int & Espacio[N][N]){
-
+// recorre el arreglo como una matriz m*n
   for (int ii = 0; ii < N; ii++) {
     for (int jj = 0; ii < N; jj++) {
       int total= Espacio[ii-1][jj]+Espacio[ii+1][jj]+Espacio[ii][jj-1]+Espacio[ii][jj+1]
+      //determina cuantos vecinos esta vivo
        Espacio[ii-1][jj-1]+Espacio[ii+1][jj+11]+Espacio[ii-11][jj-1]+Espacio[ii+11][jj+1];
-      if ((Espacio[ii][jj]=true && 2<=total<=3) || ((Espacio[ii][jj]=false && total=3)){
-          Espacio[ii][jj]=true
+       //si una celda viva, tiene entre 2 y 3 vecinos vivos celda vive
+      if (Espacio[ii][jj]=true && 2<=total<=3)
       }
-      else(){
+      //Si esta muerta y tiene 3 vecinos exactamente la celda revive
+      elif(Espacio[ii][jj]=false && total=3){
+          Espacio[ii][jj]=true)
+      //Otro Caso sa celda muere
+      else{
           Espacio[ii][jj]=0
       }
 
